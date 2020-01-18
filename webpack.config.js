c0e804836762c7564377e1f920f8d90765749ad3
Detoner <detoner777@gmail.com>
2020-01-18 21:03:08 +0200

    npm i-D html-webpack-plugin+config plugin change for html auto configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HTMLwebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -7,9 +8,14 @@ module.exports = {
         analytics: './src/analytics.js'
     },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HTMLwebpackPlugin({
+            title: 'Webpack Alexey'
+        })
+    ]
 }
 
 // to run script use cli command:
