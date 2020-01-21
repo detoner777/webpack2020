e7f908c797e1b76cc60104db4a93cca154053e30
Alexey_Lisenko <detoner777@gmail.com>
2020-01-21 22:06:11 +0200

    working with assets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import Post from './Post'
-import './styles/styles.css'
+import Post from "./Post";
+import "./styles/styles.css";
+import json from "./assets/json.json";
 
-const post = new Post('Webpack Post Title')
+const post = new Post("Webpack Post Title");
 
-console.log('Post to String', post.toString())
+console.log("Post to String", post.toString());
+
+console.log("JSON", json);
